f91915bb4337a9000d88506b35a3a57b53a4579a
abdiwoli <abdiwolih@gmail.com>
2023-07-24 18:41:00 +0300

    done

--- _putd.c
+++ _putd.c
@@ -1,10 +1,22 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
+
+int case_min(void)
+{
+	char minStr[] = "-2147483648";
+	int pos;
+
+	for (pos = 0; pos < 11; pos++)
+		_putchar(minStr[pos]);
+	return (11);
+}
+
 /**
- * _putd - function prints digits
- * @args:  va_list type
- * Return: number of digits
+ * _putd - function prints digits for %d and %i format specifiers
+ * @args: va_list type
+ * Return: number of digits printed
  */
 int _putd(va_list args)
 {
@@ -13,29 +25,26 @@ int _putd(va_list args)
 	int num = va_arg(args, int);
 	int n = 0;
 
-
 	if (num == 0)
 	{
 		n++;
 		_putchar('0');
 		return (n);
 	}
-
+	if (num == INT_MIN)
+		return (n + case_min());
 	if (num < 0)
 	{
 		n++;
 		_putchar('-');
 		num = -num;
 	}
-
-
 	while (num != 0)
 	{
 		buffer[pos] = '0' + (num % 10);
 		num /= 10;
 		pos++;
 	}
-
 	while (pos > 0)
 	{
 		pos--;
@@ -44,4 +53,3 @@ int _putd(va_list args)
 	}
 	return (n);
 }
-
