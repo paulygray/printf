5399f68b22137ddd950b4a6665d47827c970d4f1
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-07-24 08:47:23 -0700

    edit conditional in _printf

--- _printf.c
+++ _printf.c
@@ -1,21 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _printf - function that formats atguments
  * @format: string
  * Return: int
  */
-
 int _printf(const char *format, ...)
 {
 	va_list list;
 	fun_type fun;
 	int num;
-
 	num = 0;
-	va_start(list, format);
 
-	if (format == NULL)
+	va_start(list, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	while (*format)
 	{
